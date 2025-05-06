91865c926bf3c15eeba3d888a532dd18baff209e
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-07 01:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	let str=s.trim();
+	str1=str.split(" ");
+	return str1[0]
+
+	/*for(let i=0;i<str1.length;i++){
+		return str1[i]
+	}*/
 }
 
 // Do not change the code below
